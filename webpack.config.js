64e0255a336d908c1599f75e6bbf5b7cdf836081
utgwkk <utagawakiki@gmail.com>
2022-04-17 15:25:13 +0900

    control with ENABLE_SOURCEMAP environment variable

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
 const path = require('path');
 
+const enableSourceMap = process.env.ENABLE_SOURCEMAP === 'true';
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
   },
-  devtool: 'source-map',
+  devtool: enableSourceMap && 'source-map',
   module: {
     rules: [
-      {
+      enableSourceMap && {
         test: /\.js$/,
         enforce: 'pre',
         use: ['source-map-loader'],
       },
-    ],
+    ].filter(Boolean),
   },
   mode: 'production',
 };
